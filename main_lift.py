2d6487b320446c4153f678a492850203256b7b08
AdamKemp <apk208@exeter.ac.uk>
2025-02-11 11:46:01 +0000

    Added speed attribute

--- main_lift.py
+++ main_lift.py
@@ -4,11 +4,12 @@ class lift():
     """???requests could be represented as a priority queue, 
     each element may be a dictionary with floor as the key, 
     number of people and time waited as values???"""
-    
-    def __init__(self, num_floors, capacity, start = 0):
+
+    def __init__(self, num_floors, capacity, start = 0, speed = 1):
         self.num_floors = num_floors
         self.capacity = capacity
         self.y_coord = start
+        self.speed = speed
     
     
 

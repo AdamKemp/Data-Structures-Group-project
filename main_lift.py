117d86969052f1ddde74ad903835961e1251d46e
AdamKemp <apk208@exeter.ac.uk>
2025-02-17 21:34:27 +0000

    Added comments to code

--- main_lift.py
+++ main_lift.py
@@ -1,16 +1,18 @@
 class lift():
-    # Attributes include: num_floors, requests, capacity, y_coord
+    #initialise requests as an empty list
     requests = []
     """???requests could be represented as a priority queue, 
     each element may be a dictionary with floor as the key, 
     number of people and time waited as values???"""
 
     def __init__(self, num_floors, capacity, start = 0, speed = 1):
+        #initialise initial attributes
         self.__num_floors = num_floors
         self.__capacity = capacity
         self.__y_coord = start
         self.__speed = speed
     
+    #getters and setters for class attributes
     def get_floors(self):
         return self.__num_floors
     

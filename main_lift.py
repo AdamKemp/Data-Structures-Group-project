8ac19357e8ca732e048560ebbc067063b3a383e1
AdamKemp <apk208@exeter.ac.uk>
2025-02-17 21:31:26 +0000

    Added getter and setter methods

--- main_lift.py
+++ main_lift.py
@@ -6,12 +6,22 @@ class lift():
     number of people and time waited as values???"""
 
     def __init__(self, num_floors, capacity, start = 0, speed = 1):
-        self.num_floors = num_floors
-        self.capacity = capacity
-        self.y_coord = start
-        self.speed = speed
+        self.__num_floors = num_floors
+        self.__capacity = capacity
+        self.__y_coord = start
+        self.__speed = speed
     
+    def get_floors(self):
+        return self.__num_floors
     
-
-
+    def get_capacity(self):
+        return self.__capacity
     
+    def get_y(self):
+        return self.__y_coord
+    
+    def set_y(self, val):
+        self.__y_coord = val
+    
+    def get_speed(self):
+        return self.__speed
